20db3a58771474682ef188d3f606f5cce050f734
Leandro Pereira <leandrochp@gmail.com>
2023-02-25 11:52:17 -0300

    test: add unit tests registry history service

---
+++ src/test/kotlin/com/github/leandrochp/registrationhistoryservice/utils/mocks/RegistryHistoryMock.kt
@@ -0,0 +1,21 @@
+package com.github.leandrochp.registrationhistoryservice.utils.mocks
+
+import com.github.leandrochp.registrationhistoryservice.domain.registryhistory.Address
+import com.github.leandrochp.registrationhistoryservice.domain.registryhistory.RegistryHistory
+
+object RegistryHistoryMock {
+
+    fun sample() = RegistryHistory(
+        id = "123",
+        firstName = "John",
+        lastName = "Wick",
+        document = "000.000.000-00",
+        age = 44,
+        email = "babayaga@hotelcontinental.com",
+        address = Address(
+            street = "121 Mill Neck",
+            city = "Long Island",
+            code = "11765"
+        )
+    )
+}
